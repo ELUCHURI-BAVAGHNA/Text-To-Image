9d31288ab24bf1eb106f147ceb9ab2c5b374dca3
ELUCHURI BAVAGHNA <144609183+ELUCHURI-BAVAGHNA@users.noreply.github.com>
2025-08-04 00:25:17 +0530

    Update Generate.jsx

--- client/src/components/Generate.jsx
+++ client/src/components/Generate.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "framer-motion";
-import { AppContext } from "../context/appContext";
+import { AppContext } from "../context/AppContext";
 import { useNavigate } from "react-router-dom";
 const Generate = () => {
   const {user,setLogin} = useContext(AppContext)
