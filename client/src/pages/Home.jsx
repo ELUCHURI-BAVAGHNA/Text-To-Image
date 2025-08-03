4a6c3fdf8b7e631ce494807f10afca7cf80a58cd
ELUCHURI BAVAGHNA <144609183+ELUCHURI-BAVAGHNA@users.noreply.github.com>
2025-08-04 00:23:13 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
-import Description from '../components/description'
-import Testimonials from '../components/testimonials'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
 import Generate from '../components/Generate'
 
 const Home = () => {
